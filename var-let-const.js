8a4355507166c53fe7f4abe47d1b4c8c54f0d828
Joy <joydas21891920@gmail.com>
2024-02-03 13:48:25 +0600

    first commit

---
+++ var-let-const.js
@@ -0,0 +1,24 @@
+//var...
+/* 
+var name = "Joy Das";
+var age = 23;
+
+console.log(`My name is ${name} and Age is ${age}`);
+ */
+
+// let...
+
+/* 
+let name = "Joy Das";
+let age = 23;
+
+console.log(`My name is ${name} and Age is ${age}`);
+ */
+
+
+// const...
+
+const myName = "Joy Das";
+const myAge = 23;
+
+console.log(`My name is ${myName} and Age is ${myAge}`);
